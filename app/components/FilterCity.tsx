becb934ba0b0f05a4f6e18794ce808988f632219
Eduardo Moraes <eduardo.moraes@clicksoft.com.br>
2023-04-16 02:21:24 -0300

    waff

--- app/components/FilterCity.tsx
+++ app/components/FilterCity.tsx
@@ -13,6 +13,7 @@ const FilterCity = () => {
 
     const [currentCiy, setCurrentCity] = useState<City>(AllCites[0])
     const [currentState, setCurrentState] = useState<State>(AllStates[0])
+    const allCitysLocal = AllCites
 
     // Find the city in cities array
 
@@ -39,7 +40,7 @@ const FilterCity = () => {
     }
 
     const mostAlert = (value: string) => {
-        alert(AllCites.filter(city => city.id === value)[0])
+        alert(allCitysLocal.filter(city => city.id === value)[0].name)
     }
 
     return (
