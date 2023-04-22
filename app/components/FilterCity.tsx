4f8c41271fa919d58415563ec099023f936aa006
Eduardo <eduardomoraesdeveloper@gmail.com>
2023-04-22 16:14:52 -0300

    to contact

--- app/components/FilterCity.tsx
+++ app/components/FilterCity.tsx
@@ -74,12 +74,12 @@ const FilterCity = () => {
                     </select> */}
                 </div>
                 {pathname !== "/" ?
-                    <Link href="/" className='w-full'>
+                    <Link href="/contact" className='w-full'>
                         <button onClick={() => {
                             searchProperties()
                             // eslint-disable-next-line react-hooks/rules-of-hooks
                             if (pathname !== "/") {
-                                router.push("/")
+                                router.push("/contact")
                             }
                         }} className='p-3 px-4 outline-none rounded-md bg-secondary text-white font-bold shadow-md sm:w-full'>Buscar</button>
                     </Link>
